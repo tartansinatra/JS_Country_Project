5d99284bc7fb3254f2cf93fbc626a4caa29ebaed
CallumDougan <practicalitymalfunction@gmail.com>
2016-02-02 16:51:45 +0000

    functional basics, first commit

---
+++ public/app.js
@@ -0,0 +1,64 @@
+window.onload = function(){
+  console.log('App started');
+  var url = 'https://restcountries.eu/rest/v1';
+  var request = new XMLHttpRequest();
+  var countryNameList = [];
+  var dropdown = document.getElementById('Countrylist')
+  var section = document.getElementById('info')
+
+  request.open('GET', url);
+
+  var displayDropdown = function(countryNameList) {
+    for (var i = 0; i < countryNameList.length; i++) {
+      
+      var option = document.createElement("option");
+      option.innerText = countryNameList[i];
+
+      var select = document.querySelector("select");
+      select.appendChild(option);
+    };
+  }
+
+  request.onload = function() {
+    if (request.status === 200) {
+      console.log("got the data");
+      countriesData = JSON.parse(request.responseText);
+      console.log(countriesData[0]['name']);
+      
+      for (var i = 0; i < countriesData.length; i++) {
+        countryNameList.push(countriesData[i].name);
+      };
+      displayDropdown(countryNameList);
+    }
+  }
+
+  dropdown.onchange = function(){
+    var countryName = this.value;
+    var countryIndex = null;
+
+    for(index in countryNameList){
+      var testCountryName = countryNameList[index];
+      if(testCountryName === countryName){
+        var countryIndex = index;
+        console.log(countryIndex);
+        displayCountry(countryIndex);
+      }
+    }
+  }
+
+  var displayCountry = function(index){
+    var name = countriesData[index]['name'];
+    var capital = countriesData[index]['capital'];
+    var population = countriesData[index]['population'];
+
+    console.log(name, capital, population);
+
+    var blockquote = document.createElement('blockquote')
+    blockquote.innerText = (name + ' - ' + capital + ' - ' + population)
+
+    section.appendChild(blockquote)
+  }
+
+  request.send(null);
+
+};
